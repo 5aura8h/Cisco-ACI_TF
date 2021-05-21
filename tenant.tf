2e3baf7779b6e2809de41dc1aa2d84089199ea40
5aura8h <44212751+5aura8h@users.noreply.github.com>
2021-05-21 09:37:42 +0530

    first commit

--- tenant.tf
+++ tenant.tf
@@ -2,40 +2,43 @@ terraform {
   required_providers {
     aci = {
       source = "CiscoDevNet/aci"
-      version = "0.5.4"
+      version = "0.6.0"
     }
   }
+ 
 }
 
-
 provider "aci" {
-
- username = ""
- password = ""
- url = ""
+ username = "${var.user_name}"
+ #private_key = "userabc.key"
+# cert_name = "userabc.crt"
+ password = "${var.user_pwd}"
+ url = "${var.apic_url}"
+ insecure = true
 
 }
 
 resource "aci_tenant" "tenant" {
- name = "saukotha"
- description = "Using Terraform"
+ name = "${var.tenant_name}"
+ description = "${var.tenant_description}"
 }
 
 resource "aci_vrf" "vrf" {
   tenant_dn = "${aci_tenant.tenant.id}"
-  name = "vrf1"
+   name = "${var.vrf_name}"
 }
 
-resource "aci_bridge_domain" "bd" {
+resource "aci_bridge_domain" "bd100" {
   tenant_dn = "${aci_tenant.tenant.id}"
-  name = "bd1"
-  description = "using terraform"
+  description = "${var.bd100_description}"
+  name = "${var.bd100_name}"
+  relation_fv_rs_ctx = "${aci_vrf.vrf.id}"
+
 }
 
-resource "aci_subnet" "subnet" {
-  parent_dn = "${aci_bridge_domain.bd.id}"
-  description = "using terraform"
-  ip = "10.0.3.28/29"
-  scope = ["shared"]
-
+resource "aci_bridge_domain" "bd200" {
+  tenant_dn = "${aci_tenant.tenant.id}"
+  description = "${var.bd200_description}"
+  name = "${var.bd200_name}"
+  relation_fv_rs_ctx = "${aci_vrf.vrf.id}"
 }
