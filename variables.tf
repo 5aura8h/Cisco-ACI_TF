8977ede87cdd45bd91856385c9b66446e945bf85
5aura8h <44212751+5aura8h@users.noreply.github.com>
2021-05-21 09:39:37 +0530

    first commit

---
+++ variables.tf
@@ -0,0 +1,45 @@
+variable "user_name" {
+    type = string
+    default = "admin"
+}
+
+variable "user_pwd" {
+    type = string
+    default = "C1sco12345"
+}
+
+variable "apic_url" {
+    default = "https://10.106.236.61"
+
+}
+variable "tenant_name" {
+    type = string
+    default = "saukotha5"
+}
+
+variable "tenant_description" {
+    type = string
+    default = "using terraform"
+}
+
+variable "vrf_name" {
+    type = string
+    default = "vrf"
+}
+
+variable "bd100_name" {
+    type = string
+    default = "bd100"
+}
+variable "bd200_name" {
+    type = string
+    default = "bd200"
+}
+variable "bd200_description" {
+    type = string
+    default = "using terraform"
+}
+variable "bd100_description" {
+    type = string
+    default = "using terraform"
+}
